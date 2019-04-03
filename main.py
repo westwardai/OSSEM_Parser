a028b159eb12dd632d9e174f025a926c1a542ad1
zackpayton <zack.payton@westward.ai>
2019-04-03 17:29:52 +0000

    added basic unit test framework

--- main.py
+++ main.py
@@ -1,42 +1,70 @@
 #!/usr/bin/env python3
 
-import argparse, os
+import argparse, os, sys
+from functools import reduce
 import mistune
 
 __version__ = '0.0.1'
 __author__ = 'Zack Payton <zack.payton@westward.ai>'
 
 
-class YAMLRenderer(mistune.Renderer):
+class DictRenderer(mistune.Renderer):
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
+    self.object_data = {}
   #def placeholder(self):
   #  return
     #raise NotImplementedError("placeholder has not been implemented")
   def block_code(self, code, lang=None):
-    raise NotImplementedError("block_code has not been implemented")
+    print("block_code: {} lang: {}".format(code, lang))
+    #raise NotImplementedError("block_code has not been implemented")
+    return code
   def block_quote(self, text):
-    raise NotImplementedError("block_quote has not been implemented")
+    print("block_quote: {}".format(text))
+    #raise NotImplementedError("block_quote has not been implemented")
+    return text
   def block_html(self, html):
     raise NotImplementedError("block_html has not been implemented")
   def header(self, text, level, raw=None):
-    raise NotImplementedError("header has not been implemented")
+    print("header: {} level: {}".format(text, level))
+    if not 'header' in self.object_data:
+      self.object_data['headers'] = []
+    self.object_data['headers'].append({'text': text, 'level': level})
+    return text
   def hrule(self):
-    raise NotImplementedError("hrule has not been implemented")
+    print("hrule")
+    #raise NotImplementedError("hrule has not been implemented")
+    return ''
   def list(self, body, ordered=True):
-    raise NotImplementedError("list has not been implemented")
+    print("body: {} ordered: {}".format(body, ordered))
+    #raise NotImplementedError("list has not been implemented")
+    return body
   def list_item(self, text):
-    raise NotImplementedError("list_item has not been implemented")
+    print("list_item: {}".format(text))
+    #raise NotImplementedError("list_item has not been implemented")
+    return text
   def paragraph(self, text):
-    raise NotImplementedError("paragraph has not been implemented")
+    print("paragraph: {}".format(text))
+    #raise NotImplementedError("paragraph has not been implemented")
+    return text
   def table(self, header, body):
-    raise NotImplementedError("table has not been implemented")
+    print("table header: {} table body: {}".format(header, body))
+    #raise NotImplementedError("table has not been implemented")
+    return body
   def table_row(self, content):
-    raise NotImplementedError("table_row has not been implemented")
+    print("table_row: {}".format(content))
+    #raise NotImplementedError("table_row has not been implemented")
+    return content
   def table_cell(self, content, **flags):
-    raise NotImplementedError("table_cell has not been implemented")
+    print("table_cell: {}".format(content))
+    if not "table_cells" in self.object_data:
+      self.object_data['table_cells'] = []
+      self.object_data['table_cells'].append(content)
+    return content
   def double_emphasis(self, text):
-    raise NotImplementedError("double_emphasis has not been implemented")
+    print("double_emphasis: {}".format(text))
+    #raise NotImplementedError("double_emphasis has not been implemented")
+    return text
   def emphasis(self, text):
     raise NotImplementedError("emphasis has not been implemented")
   def codespan(self, text):
@@ -50,15 +78,25 @@ class YAMLRenderer(mistune.Renderer):
     return text
     #raise NotImplementedError("text has not been implemented")
   def escape(self, text):
-    raise NotImplementedError("escape has not been implemented")
+    print("escape: {}".format(text))
+    #raise NotImplementedError("escape has not been implemented")
+    return text
   def autolink(self, link, is_email=False):
-    raise NotImplementedError("autolink has not been implemented")
+    print("link: {} is_email: {}".format(link, is_email))
+    #raise NotImplementedError("autolink has not been implemented")
+    return link
   def link(self, link, title, text):
-    raise NotImplementedError("link has not been implemented")
+    print("link: {} title: {} text: {}".format(link, title, text))
+    #raise NotImplementedError("link has not been implemented")
+    return link
   def image(self, src, title, text):
-    raise NotImplementedError("image has not been implemented")
+    print("image src: {} title: {} text: {}".format(src, title, text))
+    #raise NotImplementedError("image has not been implemented")
+    return src
   def inline_html(self, html):
-    raise NotImplementedError("inline_html has not been implemented")
+    print("inline_html: {}".format(html)) 
+    #raise NotImplementedError("inline_html has not been implemented")
+    return html
   def newline(self):
     raise NotImplementedError("newline has not been implemented")
   def footnote_ref(self, key, index):
@@ -70,39 +108,47 @@ class YAMLRenderer(mistune.Renderer):
 
 
 
+class OSSEMParser(object):
+  def parse_md_file(self, filename):
+    ''' parse a markdown file '''
+    try:
+      with open(filename) as file:
+        file_content = file.read()
+        self.parse_md(file_content)
+    except FileNotFoundError as e:
+        print("File not found: {0}".format(filename))
 
-def parse_md_file(filename):
-  ''' parse a markdown file '''
-  try:
-    with open(filename) as file:
-      file_content = file.read()
-      parse_md(file_content)
-  except FileNotFoundError as e:
-      print("File not found: {0}".format(filename))
+  def parse_md(self, markdown):
+    dict_renderer = DictRenderer()
+    print(mistune.markdown(markdown, renderer=dict_renderer))
 
-def parse_md(markdown):
-  yaml_renderer = YAMLRenderer()
-  print(mistune.markdown(markdown, renderer=yaml_renderer))
-
-def parse_ossem(ossem_dir):
-  for root, subdirs, files in os.walk(ossem_dir):
-    subdirs[:] = [d for d in subdirs if not d.startswith('.')] # skip hidden directories
-    files =  [f for f in files if f.lower().endswith('.md')] # only process markdown files
-    for f in files:
-      full_file_path = os.path.join(root, f)
-      if full_file_path.endswith(os.path.join('OSSEM', 'README.md')): continue # skip the main OSSEM/README.md for now
-      print("parsing: {}".format(full_file_path))
-      ds = parse_md_file(full_file_path)
-      print("ds: {}".format(ds))
-      import sys
-      sys.exit()
-
+  def parse_ossem(self, ossem_dir):
+    d = {} # data stucture to maintain representation of OSSEM
+    start = ossem_dir.rfind(os.sep) + 1
+    for root, subdirs, files in os.walk(ossem_dir):
+      subdirs[:] = [d for d in subdirs if not d.startswith('.')] # skip hidden directories
+      files[:] = [f for f in files if not f.startswith('.') and not f.endswith('png')]
+      folders = root[start:].split(os.sep)  # http://code.activestate.com/recipes/577879-create-a-nested-dictionary-from-oswalk/
+      subdir = dict.fromkeys(files)         # this code is discusting and needs to be refactored
+      parent = reduce(dict.get, folders[:-1], d) # but it works :(
+      parent[folders[-1]] = subdir
+      for f in files:
+        full_path = os.path.join(root, f)
+        if full_path.endswith('OSSEM/README.md'): continue # skip OSSEM/README.md
+        print("parsing {}".format(full_path))
+        self.parse_md_file(full_path)
+    from pprint import pprint
+    pprint(d)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='parse markdown file')
   parser.add_argument('--ossem', type=str, help='base directory containing the OSSEM project')
   args = parser.parse_args()
-  print(args)
 
   if args.ossem:
-    parse_ossem(args.ossem)
+    parser = OSSEMParser()
+    ossem = parser.parse_ossem(args.ossem)
+  else:
+    import unittest
+    from tests.test_cim import TestOSSEMCIM
+    unittest.main()
