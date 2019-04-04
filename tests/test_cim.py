e57910cabf3f01c2019dc96acc41ded513305a2d
zackpayton <zack.payton@westward.ai>
2019-04-04 20:06:58 +0000

    fixed test criteria with input from Carl

--- tests/test_cim.py
+++ tests/test_cim.py
@@ -14,14 +14,41 @@ class TestOSSEMCIM(unittest.TestCase):
     desired_output = {
       'name': 'Alert Schema',
       'description': 'Alert fields that describe an indicator from a tool of a possible issue.',
-      'data_fields': [
-        { 'standard_name': 'alert_id', 'type': 'integer', 'description': 'Alert ids might repeat across different data sources', 'sample_value': 1234 },
-        { 'standard_name': 'alert_signature', 'type': 'string', 'description': 'The name or title of an alert', 'sample_value': 'EvilActor:CnCv2' },
-        { 'standard_name': 'alert_message', 'type': 'string', 'description': 'The message provided by the alert', 'sample_value': 'A file exhibiting behaviour of the evilactor command and control framework 2 was detected.' },
-        { 'standard_name': 'alert_description', 'type': 'string', 'description': 'The expanded description of the event', 'sample_value': '...' },
-        { 'standard_name': 'alert_severity', 'type': 'string', 'description': 'The severity of an alert', 'sample_value': 'Priority 5' },
-        { 'standard_name': 'alert_category', 'type': 'string', 'description': 'The category of an alert', 'sample_value': 'Malware' },
-        { 'standard_name': 'alert_version', 'type': 'string', 'description': 'A signature or alert version', 'sample_value': '1.2' }
-      ]
+      'data_fields': {
+        'alert_id': {
+          'type': 'integer',
+          'description': 'Alert ids might repeat across different data sources',
+          'sample_value': 1234
+        },
+        'alert_signature': {
+          'type': 'string',
+          'description': 'The name or title of an alert',
+          'sample_value': 'EvilActor:CnCv2'
+        },
+        'alert_message': { 'type': 'string',
+          'description': 'The message provided by the alert',
+          'sample_value': 'A file exhibiting behaviour of the evilactor command and control framework 2 was detected.'
+        },
+        'alert_description': {
+          'type': 'string',
+          'description': 'The expanded description of the event',
+          'sample_value': '...'
+        },
+        'alert_severity': {
+          'type': 'string',
+          'description': 'The severity of an alert',
+          'sample_value': 'Priority 5'
+        },
+        'alert_category': {
+          'type': 'string',
+          'description': 'The category of an alert',
+          'sample_value': 'Malware'
+        },
+        'alert_version': {
+          'type': 'string',
+          'description': 'A signature or alert version',
+          'sample_value':'1.2'
+        }
+      }
     }
     assert(desired_output == self.p.parse_md_file(self.alert_md))
